9f3ed7fc710a24f11c657620d2c8d5484a4ca888
Vítor Oliveira <vitorsk807@gmail.com>
2023-10-18 23:50:20 -0300

    Criando pastas e adcionando novo script para execução

--- Aut_Dps.ps1
+++ Aut_Dps.ps1
@@ -2,7 +2,7 @@
 $isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
 # Se não estiver sendo executado como administrador, solicita privilégios de administrador e reinicia o script
-if (-not $isAdmin) { 
+if (-not $isAdmin) {   
     Start-Process powershell.exe -ArgumentList "Start-Process PowerShell -Verb RunAs -ArgumentList '-File $($MyInvocation.MyCommand.Path)'" -Verb RunAs
     exit
 }
@@ -12,7 +12,7 @@ netsh interface ipv6 set privacy state=disabled
 
 # Habilita o Remote Desktop
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop" 
 
 # Saída para indicar a conclusão 
 Write-Host "A privacidade do IPv6 foi desativada e o Remote Desktop foi habilitado com sucesso."
